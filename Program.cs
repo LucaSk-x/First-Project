24143806f16f3bea21c30430ab8214d333a9ee00
LucaSk-x <lucapiccinno64@gmail.com>
2022-10-27 10:41:00 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] cibiPreferiti = {"Pizza", "Pasta", "Risotto", ""};
+
+for (int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i]);
+}
+
+for (int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i]);
+}
